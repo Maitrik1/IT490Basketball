3b9af670b4dc9086b4710fe44cbb7a3f6ae5399e
Chinonso Anyanka <cc792@njit.edu>
2021-12-06 12:47:37 -0500

    Added responsiveness to players.php

--- dist/players.php
+++ dist/players.php
@@ -47,6 +47,48 @@ if (isset($_GET['page'])) {
 </head>
 
 <body>
+<div class="container-fluid">
+    <div class="row">
+        <nav class="navbar navbar-expand-md
+                    justify-content-start
+                    bg-dark navbar-dark
+                    col-12">
+
+            <!-- Brand -->
+            <a class="navbar-brand" href="index.html">Hoop Squad</a>
+            <!-- Responsive Button for the smaller menu -->
+            <button class="navbar-toggler" type="button"
+                    data-toggle="collapse" data-target="#collapsibleNavbar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="collapsibleNavbar">
+                <ul class="navbar-nav">
+                    <!-- Links -->
+                    <!-- Dropdown -->
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbardrop"
+                           data-toggle="dropdown">Games</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Games</a>
+                            <a class="dropdown-item" href="#">Achievements</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item">
+                        <a class="nav-link" href="players.php">Stats</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="login.php">Account</a>
+                    </li>
+                    <li class="nav-item">
+                        <input type="text" placeholder="Search..">
+                        <button type="submit">Search</button>
+                    </li>
+
+                </ul>
+            </div>
+        </nav>
+    </div>
     <div class="container">
         <h1>Players!</h1>
         <div class="row">
