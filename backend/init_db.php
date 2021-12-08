5e1dfb7219e68a435b76cda934c088837f109699
Chinonso Anyanka <cc792@njit.edu>
2021-12-08 12:42:45 -0500

    Updated init_db.php

--- backend/init_db.php
+++ backend/init_db.php
@@ -1,3 +1,17 @@
 <?php
+require "config.php";
+
+$dsn = "mysql:host=localhost;dbname=Hoop Squad;charset=UTF8";
+
+try {
+    $pdo = new PDO($dsn,$user,$pass);
+
+    if($pdo){
+        echo "Connected to the database successfully";
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
 
 ?>
