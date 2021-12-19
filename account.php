3632fe7bed2d77026901682da2424dc8bc784280
Chinonso Anyanka <cc792@njit.edu>
2021-12-19 15:53:59 -0500

    forums

---
+++ account.php
@@ -0,0 +1,5 @@
+<?php
+$hostname = "sql1.njit.edu"     ; 	  // or "sql2.njit.edu"   OR "sql1.njit.edu"
+$username = "cc792" ;
+$project  = "cc792" ;
+$password = "Oscarblu!72" ;
--- forum.php
+++ forum.php
@@ -1,5 +1,11 @@
 <?php
-
+include (  "account.php"     ) ;
+$db = mysqli_connect($hostname, $username, $password, $project);
+if (mysqli_connect_errno())
+{ echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
+mysqli_select_db( $db, $project );
 ?>
 <html lang="en">
 <head>
